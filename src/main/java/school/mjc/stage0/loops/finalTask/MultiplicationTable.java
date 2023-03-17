14b75b6795638d5bcfed6e2afe7d93b35b93aaad
Ani <ani.mkrtchyan1982@gmail.com>
2023-03-17 20:42:12 +0400

    final task

--- src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
@@ -1,6 +1,15 @@
 package school.mjc.stage0.loops.finalTask;
 
 public class MultiplicationTable {
-    public void printTable(int numberTableToPrint){
+    public void printTable(int numberTableToPrint) {
+        for (int i = 0; i <= 10; i++) {
+            int result = numberTableToPrint * i;
+            System.out.println(i + " x " + numberTableToPrint + " = " + result);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        new MultiplicationTable().printTable(10);
     }
 }
